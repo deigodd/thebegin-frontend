d4264b27b4d800f2d52d7083e7d14fee4755248a
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-11-03 19:30:54 -0300

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>&copy; 2024 The Begin. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
