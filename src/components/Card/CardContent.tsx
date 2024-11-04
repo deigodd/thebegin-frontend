110a5f3043d8ac2c564851e6f02917a8c28cef2c
Diego Muñoz <diego.munoz11@mail.udp.cl>
2024-11-03 21:54:18 -0300

    Components Card CardContent Create

---
+++ src/components/Card/CardContent.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react';
+
+interface CardContentProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export const CardContent: React.FC<CardContentProps> = ({ children, className = '' }) => {
+  return (
+    <div className={`px-6 py-4 ${className}`}>
+      {children}
+    </div>
+  );
+};
