fdf26360658c74c054d5d28d4834fc173e4a3b75
Diego Muñoz <diego.munoz11@mail.udp.cl>
2024-11-04 00:23:54 -0300

    App Update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import MembershipPage from './pages/Membership';
+import TheBeginProps from './pages/Beggin2';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/membership" element={<MembershipPage />} />
+        <Route path="/thebegin" element={<TheBeginProps />} />
       </Routes>
     </Router>
   );
