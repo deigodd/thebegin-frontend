aa23eaf9df8138cfbfd172876ce1ffda17301567
Diego Muñoz <diego.munoz11@mail.udp.cl>
2024-11-04 00:26:01 -0300

    App update2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import MembershipPage from './pages/Membership';
-import TheBeginProps from './pages/Beggin2';
+import TheBeginProps from './pages/TheBegin';
 
 const App: React.FC = () => {
   return (
