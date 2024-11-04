7bf8c6e986734891bd0e82fea41815d4b35a497a
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-11-03 23:55:24 -0300

    App changes
    Home -> marketplace

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 // src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import Marketplace from './pages/Marketplace';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Marketplace />} />
       </Routes>
     </Router>
   );
