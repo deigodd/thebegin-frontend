f3d7a5c9b92c4b25f1f7340f7239f286c4d8a6dd
Diego Muñoz <diego.munoz11@mail.udp.cl>
2024-11-03 21:53:05 -0300

    App update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import MembershipPage from './pages/Membership';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/membership" element={<MembershipPage />} />
       </Routes>
     </Router>
   );
