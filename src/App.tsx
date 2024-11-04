a70caafc59156aa6f65b085911fc3e2e3894113e
Diego <daiscore@gmail.com>
2024-11-04 11:29:28 -0300

    add pagetitle for pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Marketplace from './pages/Marketplace';
+import PageTitle from './components/App/PageTitle';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Marketplace />} />
+        <Route path="/" element={
+          <>
+            <Marketplace />
+            <PageTitle title="Marketplace" />
+          </>
+        } />
       </Routes>
     </Router>
   );
