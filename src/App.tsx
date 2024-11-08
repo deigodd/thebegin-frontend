5aa93b0065ebfbb9a615864a43793157b7219283
Diego Muñoz <diego.munoz11@mail.udp.cl>
2024-11-08 00:34:32 -0300

    Update App Highlight

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Marketplace from './pages/Marketplace';
 import PageTitle from './components/App/PageTitle';
 import Activity from './pages/Activity';
+import Highlight from './pages/Highlight';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-<<<<<<< Updated upstream
-=======
         <Route path="/" element={<Marketplace />} />
         <Route path="/highlights" element={<Highlight />} />
         <Route path="/Activity" element={<Activity />} />
->>>>>>> Stashed changes
         <Route path="/" element={
           <>
             <Marketplace />
             <PageTitle title="Marketplace" />
-<<<<<<< Updated upstream
-=======
             <Highlight />
             <PageTitle title="Highlight" />
             <Activity />
             <PageTitle title="Activity" />
->>>>>>> Stashed changes
           </>
         } />
       </Routes>
