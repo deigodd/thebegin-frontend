48e5f41c63089e400261966309bd79317fe844b6
Aaron Pozas <samuelpzoas7@gmail.com>
2024-11-08 00:16:41 -0300

    Profile Bases

--- src/components/UserOptions.tsx
+++ src/components/UserOptions.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Navbar: React.FC = () => {
+const UserOptions: React.FC = () => {
   return (
     <nav className="bg-gradient-to-r from-gray-100 to-gray-300 p-4 flex justify-end items-right text-black">
       <div className="flex items-center space-x-4">
@@ -13,4 +13,4 @@ const Navbar: React.FC = () => {
   );
 };
 
-export default Navbar;
+export default UserOptions;
