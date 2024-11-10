1c3b4fbb17397cc4ba8b4c51f72c452b66dc0e52
Diego <daiscore@gmail.com>
2024-11-09 21:09:00 -0300

    chore: save home for test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const location = useLocation();
+
+  useEffect(() => {
+    // Aqui verificamos si hay token, despúes hay que implementar para validar si el token esta vigente -> TO-DO
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -12,47 +21,131 @@ const Navbar: React.FC = () => {
   const isActive = (path: string) => location.pathname === path;
 
   const activeStyle = {
-    color: '#7c513a',
-    fontWeight: 'bold'
+    color: "#7c513a",
+    fontWeight: "bold",
   };
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-[#d2bb90]">
+    <nav className="border-gray-200 bg-[#d2bb90]">
+      {/*padre*/}
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://www.thebegin.org/wp-content/uploads/2023/12/Recurso-25@15x-1024x273.png" className="h-12" alt="The Begin Logo" />
+          <img
+            src="https://www.thebegin.org/wp-content/uploads/2023/12/Recurso-25@15x-1024x273.png"
+            className="h-12"
+            alt="The Begin Logo"
+          />
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="text-white focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center bg-[#a56441] hover:bg-[#7c513a] ">Ingresa Aquí</button>
-          <button onClick={toggleMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-[#a56441] dark:focus:ring-white" aria-controls="navbar-cta" aria-expanded={isMenuOpen}>
+          {isLoggedIn ? (
+            <a
+              href="/mi-perfil"
+              className="text-white focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center bg-[#a56441] hover:bg-[#7c513a]"
+            >
+              MI PERFIL
+            </a>
+          ) : (
+            <a
+              href="/login"
+              className="text-white focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center bg-[#a56441] hover:bg-[#7c513a]"
+            >
+              INICIAR SESIÓN
+            </a>
+          )}
+          <button
+            onClick={toggleMenu}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-[#a56441] dark:focus:ring-white"
+            aria-controls="navbar-cta"
+            aria-expanded={isMenuOpen}
+          >
             <span className="sr-only">Abre el menú</span>
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
             </svg>
           </button>
         </div>
-        <div className={`items-center justify-between ${isMenuOpen ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`} id="navbar-cta">
+        <div
+          className={`items-center justify-between ${
+            isMenuOpen ? "block" : "hidden"
+          } w-full md:flex md:w-auto md:order-1`}
+          id="navbar-cta"
+        >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-[#D1B190] md:bg-[#d2bb90]">
             <li>
-              <a href="/" className={`block py-2 px-3 md:p-0 text-white hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`} style={isActive('/') ? activeStyle : {}}>Home</a>
+              <a
+                href="/"
+                className={`block py-2 px-3 md:p-0 text-white hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`}
+                style={isActive("/") ? activeStyle : {}}
+              >
+                HOME
+              </a>
             </li>
             <li>
-              <a href="/the-begin" className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`} style={isActive('/the-begin') ? activeStyle : {}}>The Begin</a>
+              <a
+                href="/the-begin"
+                className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`}
+                style={isActive("/the-begin") ? activeStyle : {}}
+              >
+                THE BEGIN
+              </a>
             </li>
             <li>
-              <a href="/pilares" className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`} style={isActive('/pilares') ? activeStyle : {}}>Pilares</a>
+              <a
+                href="/pilares"
+                className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`}
+                style={isActive("/pilares") ? activeStyle : {}}
+              >
+                PILARES
+              </a>
             </li>
             <li>
-              <a href="/begin-fest" className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`} style={isActive('/begin-fest') ? activeStyle : {}}>Begin Fest</a>
+              <a
+                href="/begin-fest"
+                className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`}
+                style={isActive("/begin-fest") ? activeStyle : {}}
+              >
+                BEGIN FEST
+              </a>
             </li>
             <li>
-              <a href="/trueque" className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`} style={isActive('/trueque') ? activeStyle : {}}>Trueque</a>
+              <a
+                href="/trueque"
+                className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`}
+                style={isActive("/trueque") ? activeStyle : {}}
+              >
+                TRUEQUE
+              </a>
             </li>
             <li>
-              <a href="/marketplace" className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`} style={isActive('/marketplace') ? activeStyle : {}}>Marketplace</a>
+              <a
+                href="/marketplace"
+                className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`}
+                style={isActive("/marketplace") ? activeStyle : {}}
+              >
+                MARKETPLACE
+              </a>
             </li>
             <li>
-              <a href="/contacto" className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`} style={isActive('/contacto') ? activeStyle : {}}>Contacto</a>
+              <a
+                href="/contacto"
+                className={`block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#7c513a]`}
+                style={isActive("/contacto") ? activeStyle : {}}
+              >
+                CONTACTO
+              </a>
             </li>
           </ul>
         </div>
