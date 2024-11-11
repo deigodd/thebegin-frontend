a8d9d5630a5d8784a2503dbfe528dfc264bfe06c
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-11-11 13:59:29 -0300

    Product card update

--- src/components/ProductCards/SmallProductCard.tsx
+++ src/components/ProductCards/SmallProductCard.tsx
@@ -23,21 +23,21 @@ const SmallProductCard: React.FC<ProductCardProps> = ({ product }) => {
     const { name, price, originalPrice, discount, imageUrl } = product;
 
     return (
-        <div className="border rounded-lg shadow-md p-4 w-64">
+        <div className="border bg-white rounded-sm shadow-md p-4 w-64">
             <div className="relative">
-                <img src={imageUrl} alt={name} className="w-full h-32 object-cover rounded-t-lg" />
+                <img src={imageUrl} alt={name} className="w-full h-32 object-cover rounded-t-sm" />
                 {discount && (
-                    <span className="absolute top-2 right-2 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-full">
+                    <span className="absolute top-2 right-2 bg-tbc-primaryred-600 text-white text-xs font-bold px-2 py-1 rounded-full">
                         -{discount}%
                     </span>
                 )}
             </div>
             <div className="mt-2">
                 <h3 className="text-gray-800 text-lg font-medium">{name}</h3>
-                <div className="mt-1 text-gray-900 font-semibold text-xl">
+                <div className="mt-1 text-tbc-primaryred-600 font-semibold text-xl">
                     USD {price}
                     {originalPrice && (
-                        <span className="text-gray-500 text-sm line-through ml-2">
+                        <span className="text-tbc-secondarygray-600 text-sm line-through ml-2">
                             USD {originalPrice}
                         </span>
                     )}
