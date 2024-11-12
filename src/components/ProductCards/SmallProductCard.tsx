44af979120ac0b4e44ee6ab7cd3cbb0fe62ae905
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-11-11 22:48:59 -0300

    Update SmallProductCard.tsx

--- src/components/ProductCards/SmallProductCard.tsx
+++ src/components/ProductCards/SmallProductCard.tsx
@@ -23,11 +23,11 @@ const SmallProductCard: React.FC<ProductCardProps> = ({ product }) => {
     const { name, price, originalPrice, discount, imageUrl } = product;
 
     return (
-        <div className="border bg-white rounded-sm shadow-md p-4 w-64">
+        <div className="border bg-white rounded-sm shadow-md px-4 py-5 w-64">
             <div className="relative">
-                <img src={imageUrl} alt={name} className="w-full h-32 object-cover rounded-t-sm" />
+                <img src={imageUrl} alt={name} className="w-full h-64 object-cover rounded-t-sm" />
                 {discount && (
-                    <span className="absolute top-2 right-2 bg-tbc-primaryred-600 text-white text-xs font-bold px-2 py-1 rounded-full">
+                    <span className="absolute -top-2 -right-2 bg-tbc-primaryred-600 text-white text-sm font-bold px-2 py-4 rounded-full">
                         -{discount}%
                     </span>
                 )}
