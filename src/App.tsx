fc5ca8c431b85994c9b28eb77c426106d1f2cf0f
Diego Muñoz <diego.munoz11@mail.udp.cl>
2024-11-11 21:18:32 -0300

    miniupdate App (ruta test)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Marketplace from './pages/Marketplace';
+import Profile2 from './pages/Profile2';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Marketplace />} />
+        <Route path="/profile" element={<Profile2 />} />
       </Routes>
     </Router>
   );
