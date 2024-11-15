e55bb54f57ad94260fc30ab58f8d23dd7f96d48c
Aaron Pozas <samuelpzoas7@gmail.com>
2024-11-15 11:44:51 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Marketplace from './pages/Marketplace';
 import PageTitle from './components/App/PageTitle';
 import Activity from './pages/Activity';
-import Highlight from './pages/Highlight';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Marketplace />} />
-        <Route path="/highlights" element={<Highlight />} />
         <Route path="/activity" element={<Activity />} />
       </Routes>
     </Router>
