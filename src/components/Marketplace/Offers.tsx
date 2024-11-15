ec71197dd7237906e1716993149a11eaacd875cd
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-11-15 14:32:28 -0300

    Offers

--- src/components/Marketplace/Offers.tsx
+++ src/components/Marketplace/Offers.tsx
@@ -3,7 +3,7 @@ import LargeCards from '../ProductCards/LargeProductCard';
 import MediumCards from '../ProductCards/MediumProductCard';
 import ExtraSmallCards from '../ProductCards/ExtraSmallProductCard';
 import Product from '../../types/Product';
-import logo from '../../assets/icons/svg/tb-icon-fill-gray.svg'
+import logo from '../../assets/icons/svg/tb-icon-fill-brown.svg'
 
 const Offers: React.FC = () => {
     const [products, setProducts] = useState<Product[]>([]);
@@ -21,29 +21,29 @@ const Offers: React.FC = () => {
         : products;
 
     return (
-        <div className="relative grid grid-cols-1 rounded-md sm:grid-flow-row-dense space-x-4 lg:grid-cols-3">
-            <div className='bg-red-600 w-72 absolute h-16 rounded-full left-4 -top-4'>
-            </div>
-        <div className="bg-white grid col-span-2 content-between">
+    <div className="relative grid grid-cols-1 gap-5 rounded-md sm:grid-flow-row-dense lg:grid-cols-3">
+        <div className='bg-red-600 w-72 absolute h-16 rounded-full z-10 left-0 -top-4'>
+        </div>
+        <div className="bg-white grid col-span-2 z-20 shadow-lg">
             {filteredProducts.slice(0, 1).map((product) => (
                 <LargeCards key={product.id} product={product} />
             ))}
 
             {/* Contenedor scrolleable para ExtraSmallCards */}
-            <div className="flex gap-4 overflow-x-auto lg:overflow-visible">
+            <div className="flex gap-3 overflow-x-auto lg:overflow-visible">
                 {filteredProducts.slice(1, 6).map((product) => (
-                    <div className="scale-95"> {/* Ajusta el ancho mínimo */}
+                    <div className="scale-90"> {/* Ajusta el ancho mínimo */}
                         <ExtraSmallCards key={product.id} product={product} />
                     </div>
                 ))}
             </div>
         </div>
-        <div className='bg-red-600 w-56 absolute h-8 rounded-b-full text-center font-bold pt-1 text-white left-4 -top-3'>
+        <div className='bg-red-600 w-56 z-30 absolute h-8 rounded-b-full pl-8 font-bold text-white left-8 -top-1'>
             OFERTAS DE HOY
         </div>
 
-        <div className="bg-white">
-            <div className='flex bg-purple-300 p-2 pl-4 text-tbc-secondarygray-600 font-bold text-center'>
+        <div className="bg-white shadow-lg">
+            <div className='flex bg-purple-300 p-2 pl-4 text-tbc-primarybrown-600 font-bold text-center'>
             <img src={logo} className='w-12'></img>
                 <span className='ml-4 pt-2'>MÁS VENDIDOS</span>
             </div>
