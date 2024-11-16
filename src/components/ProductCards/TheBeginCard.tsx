fe41cef2a0a10e96fa6a956729b572dcadc5b6e4
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-11-16 20:45:32 -0300

    The beggin Market

---
+++ src/components/ProductCards/TheBeginCard.tsx
@@ -0,0 +1,40 @@
+import Product from '../../types/Product'
+
+interface ProductCardProps {
+    product: Product;
+  }
+
+const TheBeginCard: React.FC<ProductCardProps> = ({ product }) => {
+    const { name, price, originalPrice, discount, imageUrl } = product;
+
+    return (
+        <div className="relative group bg-white w-full h-full overflow-hidden">
+            <div className="grid object-cover w-full h-full group-hover:translate-y-[-20%] transition-transform duration-500 ease-in-out"
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                }}>
+                {discount && (
+                    <span className="absolute -top-2 -right-2 bg-tbc-primaryred-600 text-white text-sm font-bold px-2 py-4 rounded-full">
+                        -{discount}%
+                    </span>
+                )}
+                <div className="bg-tbc-secondarybrown-200 rounded-lg px-4 w-full h-36 translate-y-full group-hover:scale-90 duration-700 ease-in-out self-end">
+                    <div className="mt-2">
+                        <h3 className="text-gray-800 text-md font-medium">{name}</h3>
+                        <div className="mt-1 text-tbc-primaryred-600 font-semibold text-lg">
+                            USD {price}
+                            {originalPrice && (
+                                <span className="text-tbc-secondarygray-600 text-sm line-through ml-2">
+                                    USD {originalPrice}
+                                </span>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default TheBeginCard;
