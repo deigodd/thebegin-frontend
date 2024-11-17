cd6d6d66180cc6f5163b61be265ef629a9599c0a
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-11-17 13:33:44 -0300

    The Begin Products Fix

--- src/components/Marketplace/ChangingView.tsx
+++ src/components/Marketplace/ChangingView.tsx
@@ -11,14 +11,14 @@ const ChangingView: React.FC<ChangingViewProps> = ({selectedPilar}) => {
   return (
 
               <div className='z-10 mt-2'>
-                {selectedPilar === "The Begin" && <TheBeginMarketProducts selectedCategory={0}/>}
+                {selectedPilar === "The Begin" && <TheBeginMarketProducts selectedCategory={777}/>}
                 {selectedPilar === "Arte con Sentido" && <PilarTemplate pilarId={1} />}
                 {selectedPilar === "Consumo con Sentido" && <PilarTemplate2 pilarId={2} />}
                 {selectedPilar === "Bienestar" && <PilarTemplate pilarId={3} />}
                 {selectedPilar === "Salud Mental" && <PilarTemplate2 pilarId={4} />}
                 {selectedPilar === "Medio Ambiente" && <PilarTemplate pilarId={5} />}
                 {!["The Begin", "Arte con Sentido", "Consumo con Sentido", "Bienestar", "Salud Mental", "Medio Ambiente"].includes(selectedPilar) && (
-                    <TheBeginMarketProducts selectedCategory={0}/>
+                    <TheBeginMarketProducts selectedCategory={777}/>
                 )}
               </div>         
 
