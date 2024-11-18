95eea5e8d744f0f6e0ca3165c7bdc07f55a778a7
Diego Muñoz <diego.munoz11@mail.udp.cl>
2024-11-18 17:28:00 -0300

    Create ProfileServiceProduct Profile2

--- src/components/Profile/SectionsVegan/ProfileServiceProduct.tsx
+++ src/components/Profile/SectionsVegan/ProfileServiceProduct.tsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ActivitiesPageLayout from '../Hooks/ColumnService';
+import PopupForm from '../Hooks/PopupForm'; // Asegúrate de tener este componente importado
 
-type Props = {}
+const ActivitiesPage: React.FC = () => {
+  const [isPopupOpen, setPopupOpen] = useState(false);
 
-const ProfileServiceProduct = (props: Props) => {
+  const openPopup = () => setPopupOpen(true);
+  const closePopup = () => setPopupOpen(false);
+
+  const activities = [
+    { title: 'Kit de Especias', 
+      image: 'https://cdn.pixabay.com/photo/2019/05/07/09/01/spices-4185324_1280.jpg' },
+    { title: 'Recetario', 
+      image: 'https://cdn.pixabay.com/photo/2021/09/20/06/55/spaghetti-6639970_1280.jpg' },
+    { title: 'Bolsón Saludable', 
+      image: 'https://cdn.pixabay.com/photo/2023/01/09/10/56/meal-7707134_1280.jpg' },
+    { title: 'Taller de Riego', 
+      image: 'https://cdn.pixabay.com/photo/2023/05/21/06/05/water-jet-8007873_1280.jpg' },
+    { title: 'Frutillas', 
+      image: 'https://cdn.pixabay.com/photo/2020/05/22/19/42/strawberry-5206853_1280.jpg' },
+    { title: 'Taller de Poda', 
+      image: 'https://cdn.pixabay.com/photo/2016/04/18/11/45/shear-1336374_1280.jpg' },
+    { title: 'Mermeladas Caseras', 
+      image: 'https://cdn.pixabay.com/photo/2017/09/12/20/22/jam-2743531_1280.jpg' },
+  ];
+
   return (
-    <section className="flex items-center justify-center min-h-screen bg-gray-300 p-10">
-          <div className="text-center">
-            <h2 className="text-2xl font-semibold text-gray-700 mb-4">Destacar Servicios</h2>
-            <p>Espacio para destacar productos, servicios o cualquier contenido que desee mostrar.</p>
-          </div>
-    </section>
-  )
-}
+    <div>
+      <ActivitiesPageLayout activities={activities} onMoreInfo={openPopup} />
+      <PopupForm isOpen={isPopupOpen} onClose={closePopup} />
+    </div>
+  );
+};
 
-export default ProfileServiceProduct
+export default ActivitiesPage;
