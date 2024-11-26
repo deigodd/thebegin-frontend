d2ccd8ea366fe6f13663416ef295fffe79baf359
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-11-25 21:58:08 -0300

    Thebegin Update
    actualicé vista the begin

--- src/components/TheBegin/InfoCard.tsx
+++ src/components/TheBegin/InfoCard.tsx
@@ -14,7 +14,7 @@ const InfoCard: React.FC<InfoCardProps> = ({ title, onClick }) => {
       className="flex scale-95 items-center bg-white cursor-pointer hover:bg-gray-100 shadow-md transition overflow-hidden"
     >
       <img src={imagePath} alt={title} className="h-32 w-40" />
-      <span className="p-2 text-3xl text-center mx-auto font-serif font-semibold text-gray-800">
+      <span className="p-2 sm:text-3xl text-center mx-auto font-serif font-semibold text-gray-800">
         {title}
       </span>
     </div>
