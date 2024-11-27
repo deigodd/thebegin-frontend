e3992ea6c8add2f76abd4b8b537dbc5e837c57e5
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2024-11-26 22:49:30 -0300

    aaaa

--- src/pages/Profile1.tsx
+++ src/pages/Profile1.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
-import UserOptions from '../components/UserOptions';
+
+
 import Navbar from '../components/Navbar';
-import ProfileHeader from '../components/Profile/Sections/ProfileHeader';
+
 import ProfileProjects from '../components/Profile/Sections/ProfileProjects';
 import ProfileServiceProduct from '../components/Profile/Sections/ProfileServiceProduct';
-import ProfileWorkshops from '../components/Profile/Sections/ProfileWorkshops';
+
 import ProfileTrueque from '../components/Profile/Sections/ProfileTrade';
 import ProfileMarketplace from '../components/Profile/Sections/ProfileMarketplace';
 import ProfileMembership from '../components/Profile/Sections/ProfileMembership';
@@ -12,28 +12,14 @@ import ProfileCalendar from '../components/Profile/Sections/ProfileCalendar';
 import ProfileNews from '../components/Profile/Sections/ProfileNews';
 import ProfileContact from '../components/Profile/Sections/ProfileContact';
 import ProfilePresentation from '../components/Profile/Sections/ProfilePresentation';
-import useIntersectionObserver from '../components/Profile/Hooks/useIntersectionObserver';
+
 import Footer from '../components/Footer/Footer';
 
 
 
 
 const Profile = () => {
-  const sectionRefs = {
-    presentation: useRef(null),
-    projects: useRef(null),
-    serviceProduct: useRef(null),
-    workshops: useRef(null),
-    trueque: useRef(null),
-    marketplace: useRef(null),
-    membership: useRef(null),
-    calendar: useRef(null),
-    news: useRef(null),
-    contact: useRef(null),
-  };
 
-  const sections = Object.values(sectionRefs).map((ref) => ref.current);
-  const visibleSections = useIntersectionObserver(sections, { threshold: 0.5 });
 
 
   return (
